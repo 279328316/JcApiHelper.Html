f9c0210341acf6775b1716ddd70f4aea3fadaa6e
zhangjiahua <279328316@qq.com>
2019-07-29 11:44:58 +0800

    bug fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  //SERVER_URL: 'http://beipiaohome.cn:8003/ApiHelper/',
+  SERVER_URL: 'http://jccore.cn:8003/ApiHelper/',
   //SERVER_URL: '',
   //SERVER_URL: 'http://127.0.0.1:5000/ApiHelper/',
-  SERVER_URL: 'http://debugconsole.sinounion.com/Api/ApiHelper/',
   production: false
 };
 
