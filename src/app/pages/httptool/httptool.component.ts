404e35e89f6f27eef8c23c78f22d4e9b7942eb03
zhangjiahua <279328316@qq.com>
2019-08-28 18:44:58 +0800

    添加HttpTool组件

--- src/app/pages/httptool/httptool.component.ts
+++ src/app/pages/httptool/httptool.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./httptool.component.less']
 })
 export class HttpToolComponent implements OnInit {
-
+  method:string = 'Get';
+  ecode:string = 'UTF-8';
+  contentType:string = 'application/json';
   constructor() { }
 
   ngOnInit() {
