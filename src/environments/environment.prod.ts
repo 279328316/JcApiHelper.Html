4f2d9d77c887da65a1ebe975a301690d9fd70b71
zhangjiahua <279328316@qq.com>
2019-09-06 18:26:46 +0800

    bug fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  SERVER_URL: '',
+  //SERVER_URL: '',     // for JcApiHelper Project
+  SERVER_URL: 'ApiHelper/',   // for https://apihelper.jccore.cn
   proxyServer: 'ws://127.0.0.1:7900'
 };
