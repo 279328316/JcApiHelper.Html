53261e5554422a03ca7de163481dae3fa78ec89f
jiahua.zhang <279328316@qq.com>
2021-10-21 16:43:08 +0800

    Refresh bug fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  SERVER_URL: 'http://10.10.11.51:12002/ApiHelper/',
-  //SERVER_URL: 'http://debugconsole.sinounion.com/api/',
+  SERVER_URL: 'http://127.0.0.1:8001/ApiHelper/',
   //SERVER_URL: '',
   //SERVER_URL: 'http://127.0.0.1:5000/',
   production: false
